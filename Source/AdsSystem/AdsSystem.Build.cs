03be62f316fde66200d35adb2a792b8fe89421e0
shenkns <shenkns@gmail.com>
2023-05-29 07:54:45 +0300

    Initial Commit

---
+++ Source/AdsSystem/AdsSystem.Build.cs
@@ -0,0 +1,32 @@
+// Copyright shenkns Ads System Developed With Unreal Engine. All Rights Reserved 2023.
+
+using UnrealBuildTool;
+
+public class AdsLogSystem : ModuleRules
+{
+	public AdsSystem(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateIncludePaths.AddRange(
+			new string[] {
+				"Ads System/Public/"
+			}
+		);
+
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core"
+			}
+		);
+
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine"
+			}
+		);
+	}
+}
---
+++ Source/AdsSystem/Private/Module/AdsSystemModule.cpp
@@ -0,0 +1,5 @@
+// Copyright shenkns Ads System Developed With Unreal Engine. All Rights Reserved 2023.
+
+#include "Module/AdsSystemModule.h"
+
+IMPLEMENT_MODULE(FAdsSystemModule, AdsSystem)
